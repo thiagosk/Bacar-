c756e4c1044581135ab13a805ba508d9458cba83
thiagosk <thiagosk1@al.insper.edu.br>
2020-09-28 18:54:46 -0300

    Enter

--- bacará.py
+++ bacará.py
@@ -1,1 +1,16 @@
-# Design de Software - Exercício Programa
+# Insper - Engenharia - Design de Software - Exercício Programa: Bacará 
+# Thiago Shiguero Kawahara - Turma B - 10/2020
+
+import random
+
+fdj = 50 #fichas do jogador
+
+fichas = int(input("Quantas fichas deseja apostar? "))
+fs = fdj - fichas #fs=fichas sobrando
+print("Você esta com {0} fichas agora.".format(fs))
+aposta = input("Em quem irá apostar, jogador ou mesa? ")
+
+
+
+
+            
